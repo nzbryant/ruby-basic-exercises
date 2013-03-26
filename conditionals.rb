40f2e3230c16c08fa280732966e3085544724c09
Rodney Bryant <nzbryant@yahoo.com>
2013-03-26 14:05:11 +0000

    Conditionals exercise

---
+++ conditionals.rb
@@ -0,0 +1,21 @@
+print "age?"
+age = gets.chomp.to_i
+
+if age<40
+	puts "young and dumb"
+elsif age>=40
+	puts "wisw"
+end
+
+print "color?"
+color = gets.chomp.to_s
+case color
+	when "blue"
+		puts "alright!"
+	when "red"
+		puts "aggro man!"
+	else puts "weirdo"
+end	
+
+
+	
