6570669b53f967d6bc562d7acbf22d902294a209
Rodney Bryant <nzbryant@yahoo.com>
2013-03-26 14:06:08 +0000

    Strings exercise

---
+++ strings.rb
@@ -0,0 +1,13 @@
+#What is Charles doing?
+puts "Hello Charles. What are you doing?"
+
+
+activity = gets.chomp
+
+puts "Charles is #{activity}"
+
+puts "charles are you the man?"
+
+age = gets.chomp
+
+print age
